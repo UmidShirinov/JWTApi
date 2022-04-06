bc86f5647ce420981c4478937427f350f2361168
Umid Sirinov <sirinovumid623@gmail.com>
2022-04-06 12:11:41 +0400

    Create IUnitOfWork SErvices

---
+++ AuthServer.Core/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.UnitOfWork
+{
+    public interface IUnitOfWork  // db ya productnan stocku elave eliyende 2 sin elave eliyib sora savechanges eliyirik
+    {
+        Task CommitAsync();
+        void Commit();
+    }
+}
