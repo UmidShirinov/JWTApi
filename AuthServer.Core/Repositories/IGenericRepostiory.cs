e55f3f3d4e921dfac6017db23e47ccf878788958
Umid Sirinov <sirinovumid623@gmail.com>
2022-04-11 11:37:13 +0400

    Create GenericRepository  and implement  IgenericRepositry Methods

--- AuthServer.Core/Repositories/IGenericRepostiory.cs
+++ AuthServer.Core/Repositories/IGenericRepostiory.cs
@@ -11,7 +11,6 @@ namespace AuthServer.Core.Repositories
     {
         Task<TEntity> GetByIdAsync(int id);
         Task<IEnumerable<TEntity>> GetAllAsync();
-
         IQueryable<TEntity> Where(Expression<Func<TEntity, bool>> predicate);
         Task AddAsync(TEntity entity);
         void Remove(TEntity entity);
