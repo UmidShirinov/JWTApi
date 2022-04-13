3546a8e6ee7247bbb11891abc2554ec2eebc11f5
Umid Sirinov <sirinovumid623@gmail.com>
2022-04-13 11:43:20 +0400

    Adding  Mapper

---
+++ AuthServer.Service/DtoMapper.cs
@@ -0,0 +1,16 @@
+﻿using AuthServer.Core.DTOS;
+using AuthServer.Core.Model;
+using AutoMapper;
+
+namespace AuthServer.Service
+{
+    public class DtoMapper:Profile
+    {
+        public DtoMapper()
+        {
+            CreateMap<ProductDto, Product>().ReverseMap();
+            CreateMap<UserAppDto, UserApp>().ReverseMap();
+        }
+
+    }
+}
---
+++ AuthServer.Service/ObjectMapper.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using System;
+
+namespace AuthServer.Service
+{
+    public static class ObjectMapper
+    {
+        private static readonly Lazy<IMapper> lazy = new Lazy<IMapper>(() =>
+        {
+
+            var config = new MapperConfiguration(cfg=>
+                cfg.AddProfile<DtoMapper>()
+            );
+
+            return config.CreateMapper(); 
+
+            // lazy method Projet run olunanda Memorye yazilmir.
+        });
+
+        public static IMapper Mapper => lazy.Value;
+    }
+}
