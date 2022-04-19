f13526b81cdfc7aeb96dbc7137c7a2ff5fb1fe76
Umid Sirinov <sirinovumid623@gmail.com>
2022-04-19 10:56:47 +0400

    Creating TokenService For RefreshToken

---
+++ AuthServer.Service/Services/TokenService.cs
@@ -0,0 +1,57 @@
+﻿using AuthServer.Core.Configuration;
+using AuthServer.Core.DTOS;
+using AuthServer.Core.Model;
+using AuthServer.Core.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using SharedLibrary.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Service.Services
+{
+    public class TokenService : ITokenService
+    {
+        public UserManager<UserApp> _userManager { get; set; }
+        public CustomTokenOption _tokenOption { get; set; }
+
+
+
+        public TokenService(UserManager<UserApp> _userManager,IOptions<CustomTokenOption> _tokenOption)
+        {
+            this._tokenOption = _tokenOption.Value;
+            this._userManager = _userManager;
+
+        }
+
+
+
+        // Random  stringin alinmasi.
+        private string CreateRefreshToken()
+        {
+            var numberByte = new Byte[32];
+
+            var rndm = RandomNumberGenerator.Create();
+
+            rndm.GetBytes(numberByte);
+
+            return Convert.ToBase64String(numberByte);
+
+        }
+        public TokenDto CreateToken(UserApp userApp)
+        {
+            throw new NotImplementedException();
+
+            
+        }
+
+        public ClientTokenDto CreateTokenByClient(Client client)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
