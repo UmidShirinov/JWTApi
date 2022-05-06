c661890cfdd711d1a1d730c1fc520e4219e46070
Umid Sirinov <sirinovumid623@gmail.com>
2022-05-06 15:05:31 +0400

    Create AuthController

---
+++ JWTApi/Controllers/AuthController.cs
@@ -0,0 +1,55 @@
+﻿using AuthServer.Core.DTOS;
+using AuthServer.Core.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JWTApi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AuthController : BaseeController
+    {
+        private readonly IAutentiocationService _autentiocationService;
+
+        public AuthController(IAutentiocationService _autentiocationService)
+        {
+            this._autentiocationService = _autentiocationService;
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> CreateToken(LoginDto loginDto)
+        {
+            var result = await _autentiocationService.CreateAccessToken(loginDto);
+            return ActionResultInstance(result);
+        }
+
+        [HttpPost]
+        public IActionResult CreateTokenByClient(ClientLoginDto clientLoginDto)
+        {
+            var result = _autentiocationService.CreateTokenByClient(clientLoginDto);
+
+            return ActionResultInstance(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RevokeRefreshToken(RefreshTokenDto refreshTokenDto)
+        {
+            var result = await _autentiocationService.RevokeRefreshToken(refreshTokenDto.Token);
+
+            return ActionResultInstance(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateTokenByRefreshToken(RefreshTokenDto refreshTokenDto)
+        {
+            var result = await _autentiocationService.CreateTokenByRefreshToken(refreshTokenDto.Token);
+
+            return ActionResultInstance(result);
+        }
+    }
+}
---
+++ JWTApi/Controllers/BaseeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SharedLibrary.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JWTApi.Controllers
+{
+    public class BaseeController : ControllerBase
+    {
+        public IActionResult ActionResultInstance<T>(Response<T> response ) where T: class
+        {
+            return new ObjectResult(response)
+            { 
+                StatusCode = response.StatusCode
+                
+            }                            ;
+        }
+    }
+}
